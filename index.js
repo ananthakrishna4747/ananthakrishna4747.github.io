ec798be7745d6e0af1096ee537b0e7fd65a09735
ananthakrishna4747 <akshaychilappa47@gmail.com>
2024-06-27 12:28:28 -0500

    moved index.js out of src/components

---
+++ index.js
@@ -0,0 +1,32 @@
+// export { default as Head } from './head';
+// export { default as Layout } from './layout';
+// export { default as Loader } from './loader';
+// export { default as Nav } from './nav';
+// export { default as Menu } from './menu';
+// export { default as Side } from './side';
+// export { default as Social } from './social';
+// export { default as Email } from './email';
+// export { default as Footer } from './footer';
+// export { default as Hero } from './sections/hero';
+// export { default as About } from './sections/about';
+// export { default as Jobs } from './sections/jobs';
+// export { default as Featured } from './sections/featured';
+// export { default as Projects } from './sections/projects';
+// export { default as Contact } from './sections/contact';
+
+
+export { default as Head } from './src/components/head';
+export { default as Layout } from './src/components/layout';
+export { default as Loader } from './src/components/loader';
+export { default as Nav } from './src/components/nav';
+export { default as Menu } from './src/components/menu';
+export { default as Side } from './src/components/side';
+export { default as Social } from './src/components/social';
+export { default as Email } from './src/components/email';
+export { default as Footer } from './src/components/footer';
+export { default as Hero } from './src/components/sections/hero';
+export { default as About } from './src/components/sections/about';
+export { default as Jobs } from './src/components/sections/jobs';
+export { default as Featured } from './src/components/sections/featured';
+export { default as Projects } from './src/components/sections/projects';
+export { default as Contact } from './src/components/sections/contact';
